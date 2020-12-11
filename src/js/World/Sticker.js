52007fe0b2a4a2ab02adc3e9b101610c9a85e8ad
Alexis <alexis.sejourne@hotmail.com>
2020-12-11 10:54:22 +0100

    Decalco drawed, need to set the positions

--- src/js/World/Sticker.js
+++ src/js/World/Sticker.js
@@ -2,7 +2,7 @@ import { Object3D, MeshBasicMaterial, Mesh, Vector3, Euler } from 'three'
 import * as THREE from 'three'
 import { DecalGeometry } from 'three/examples/jsm/geometries/DecalGeometry.js';
 
-export default class Bottle {
+export default class Sticker {
   constructor(options) {
     // Options
     this.time = options.time
@@ -21,21 +21,21 @@ export default class Bottle {
     
 
     const box = new THREE.BoxGeometry( 1, 1, 1 );
-    console.log(box)
-    const material = new MeshBasicMaterial( { color: 0x0000ff } );
+    const material = new MeshBasicMaterial( { color: 0xff0000 } );
     const meshBox = new Mesh( box, material );
     const mesh3D = new Mesh( this.bottle.bottle.shape.geometry, material );
     console.log(meshBox);
     console.log(mesh3D);
     mesh3D.scale.set(0.05, 0.05, 0.05)
-    this.container.add(mesh3D);
+    //this.container.add(mesh3D);
     
-    const position = new THREE.Vector3(1, 1, 1);
-    const orientation = new THREE.Euler(1,1,1,1, 'ui');
-    const size = new THREE.Vector3( 1, 1, 1 );
-    const geometry = new DecalGeometry( meshBox, position, orientation, size );
-    const material2 = new MeshBasicMaterial( { color: 0x00ff00 } );
+    const position = new THREE.Vector3(5, 1, 0);
+    const orientation = new THREE.Euler(10,0,0,0, 'ui');
+    const size = new THREE.Vector3( 50, 20, 20 );
+    const geometry = new DecalGeometry( mesh3D, position, orientation, size );
+    const material2 = new MeshBasicMaterial( { color: 0x0000ff } );
     const mesh2 = new Mesh( geometry, material2 );
+    mesh2.scale.set(0.05, 0.05, 0.05)
 
     this.container.add( mesh2 );
   }
